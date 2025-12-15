5484db230cd31214c1dc0e3935d83eeee998b09f
dheshva2020 <dheshva2020@gmail.com>
2025-12-14 22:29:20 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+class Product {
+    constructor(parameters) {
+        this.parameters = parameters;
+        this.name = name;
+        this.price = 0;
+        this.quantity = 0;          
+    }
